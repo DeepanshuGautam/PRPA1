f754f3386d1031e5270aa9afae4d513e93d3799c
Deepanshu Gautam <deepanshu09.subhash@gmail.com>
2015-03-30 13:54:17 +0530

    Revert "hiii"
    This reverts commit d59d96aeee211926f5c326ef151919dfde19c4b1.

--- adheader.php
+++ adheader.php
@@ -5,17 +5,18 @@
 
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 
-		<script type="text/javascript" src="js/jquery.min.js"></script>			
+		<script type="text/javascript" src="js/jquery.min.js"></script>	
+		<script type="text/javascript" src="js/jquery-ui.min.js"></script>
 		<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	</head>
 	<body id="body">
-		<div class="header row">
-			<div class="col-md-1"><a href="adminHome.php"><img id="logo" src="logo.png"></a></div>	
-			<div  class="name vcenter col-md-11" >
-				<div id="college_name">
+		<div id="header" class="row">
+			<div class="col-lg-2"><a href="adminHome.php"><img id="logo" src="logo.png"></a></div>	
+			<div class="col-lg-10">
+				<div class="row col-md-12 vcenter" id="college_name">
 					Indian Institute of Information Technology, Design & Manufacturing
 				</div>
-				<div id="sub_headline">(An Institute of National Importance Fully Funded by Govt. of India)											
+				<div class="row col-md-12 vcenter" id="sub_headline">(An Institute of National Importance Fully Funded by Govt. of India)											
 				</div>
 			</div> 
 		</div>
